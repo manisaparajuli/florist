8f692b6d145e2dac65b633291fb6dd489f6ffe39
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-14 22:04:41 +1000

    styles added for active links

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css';
 import logo from '../Assests/bliss-logo.png';
 import cart from '../Assests/add-to-cart.png';
+
 const Navbar = () => {
+  const [activeItem, setActiveItem] = useState('Home'); 
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -10,14 +15,15 @@ const Navbar = () => {
         <p>The florist</p>
       </div>
         <ul className="nav-menu">
-          <li>Home</li>
-          <li>Product</li>
-          <li>Wedding & Event</li>
-          <li>About Us</li>
+          <li onClick={() => handleItemClick('home')} className={activeItem === 'home' ? 'active' : ''}> Home</li>
+          <li onClick={() => handleItemClick('product')} className={activeItem === 'product' ? 'active' : ''}>Product</li>
+          <li onClick={() => handleItemClick('wedding&event')} className={activeItem === 'wedding&event' ? 'active' : ''}>Wedding & Event</li>
+          <li onClick={() => handleItemClick('aboutUs')} className={activeItem === 'aboutUs' ? 'active' : ''}>About Us</li>
         </ul>
         <div className="nav-login-cart">
           <button>Login</button>
           <img  src={cart} alt="cart" />
+          <div className='nav-cart-count'>0</div>
         </div>
     </div>
   )
