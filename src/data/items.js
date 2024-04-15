b83cf03e465c1284087044534ebfcf1f3cfbe995
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-15 13:56:14 +1000

    data for plant is created

---
+++ src/data/items.js
@@ -0,0 +1,40 @@
+import satinImage from "../Components/Assests/thirdImage.png"
+import peperomia from "../Components/Assests/Peperomia.png"
+import catathea from "../Components/Assests/calathea.png"
+import monstera from "../Components/Assests/monstera.png"
+let data = [
+  {
+    id: 1,
+    name : "Satin Pothos",
+    price : 16,
+    description : "Introducing the Satin Pothos, a stunning addition to any indoor      space. With its heart-shaped leaves featuring a lush satin-like texture and striking silver variegation, this plant adds a touch of elegance to any room. Thriving in low to medium light conditions, it's an ideal choice for busy plant enthusiasts seeking low-maintenance greenery. Whether adorning a shelf, hanging gracefully in a basket, or cascading from a planter, the Satin Pothos brings a touch of natural beauty to any home or office environment.",
+    quantity : 6,
+    image: satinImage,
+  },
+  {
+    id: 2,
+    name : "Peperomia Watermelon",
+    price : 19,
+    description : "Meet the Peperomia Watermelon, a delightful addition to your plant collection. Its vibrant green leaves, adorned with striking watermelon-like patterns, bring a playful charm to any space. Compact and easy to care for, this petite plant thrives in bright, indirect light, making it perfect for desks, shelves, or windowsills. With its unique foliage and low-maintenance nature, the Peperomia Watermelon is sure to be a conversation starter and a cherished accent in your home or office.",
+    quantity : 7,
+    image: peperomia,
+  },
+  
+  {
+    id: 4,
+    name : "Calathea Medallion",
+    price : 29,
+    description : "Introducing the Calathea Medallion - a botanical masterpiece for your indoor oasis. With its intricately patterned leaves resembling medallions of artistry, this plant captivates with its elegance and charm. Perfect for adding a touch of tropical flair to any room, the Calathea Medallion thrives in medium to bright indirect light, making it an ideal choice for well-lit living spaces or offices. With its unique ability to fold its leaves at night, it adds an interactive element to your plant collection. Elevate your space with the beauty of the Calathea Medallion, a true gem among houseplants.",
+    quantity : 2,
+    image: catathea,
+  },
+  {
+    id: 5,
+    name : "The Monstera",
+    price : 34,
+    description : "Introducing the Monstera - the epitome of botanical chic. With its iconic Swiss cheese-like leaves and unmistakable tropical charm, this plant makes a bold statement in any space. Thriving in bright, indirect light and with minimal watering needs, the Monstera is the perfect choice for both novice and seasoned plant enthusiasts alike. Whether gracing a corner of your living room or adding a touch of green to your office, its lush foliage and dramatic silhouette instantly elevate any environment. Embrace the allure of the Monstera and bring a slice of the jungle into your home.",
+    quantity : 2,
+    image: monstera,
+  }
+
+]
