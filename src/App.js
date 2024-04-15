eeebf33280b78ca28e1b0e53b40eb521669f07f9
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-15 11:16:28 +1000

    router DOM added to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About'
+import Events from './Pages/Events'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart';
+import Authentication from './Pages/Authentication';
 
 function App() {
   return (
     <div >
-      <Navbar/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/home" element={ <Home/> } />
+          <Route path="/about" element={ <About/> } />
+          <Route path="/events" element={ <Events/> } />
+          <Route path="/product" element={ <Product/> } >
+            <Route path=':productId' element={<Product/> } />
+          </Route>
+          <Route path='/cart' element={<Cart/> } />
+          <Route path='/authentication' element={<Authentication/> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css';
 import logo from '../Assests/bliss-logo.png';
 import cart from '../Assests/add-to-cart.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [activeItem, setActiveItem] = useState('Home'); 
@@ -15,14 +16,14 @@ const Navbar = () => {
         <p>The florist</p>
       </div>
         <ul className="nav-menu">
-          <li onClick={() => handleItemClick('home')} className={activeItem === 'home' ? 'active' : ''}> Home</li>
-          <li onClick={() => handleItemClick('product')} className={activeItem === 'product' ? 'active' : ''}>Product</li>
-          <li onClick={() => handleItemClick('wedding&event')} className={activeItem === 'wedding&event' ? 'active' : ''}>Wedding & Event</li>
-          <li onClick={() => handleItemClick('aboutUs')} className={activeItem === 'aboutUs' ? 'active' : ''}>About Us</li>
+          <li onClick={() => handleItemClick('home')} className={activeItem === 'home' ? 'active' : ''}><Link to="/home" style={{textDecoration: "none", color: "inherit"}}> Home</Link></li>
+          <li onClick={() => handleItemClick('product')} className={activeItem === 'product' ? 'active' : ''}><Link to="/product" style={{textDecoration: "none", color: "inherit"}}>Product</Link></li>
+          <li onClick={() => handleItemClick('wedding&event')} className={activeItem === 'wedding&event' ? 'active' : ''}> <Link to="/events" style={{textDecoration: "none", color: "inherit"}}>Wedding & Event</Link></li>
+          <li onClick={() => handleItemClick('aboutUs')} className={activeItem === 'aboutUs' ? 'active' : ''}><Link to="/about" style={{textDecoration: "none", color: "inherit"}}>About Us</Link></li>
         </ul>
         <div className="nav-login-cart">
-          <button>Login</button>
-          <img  src={cart} alt="cart" />
+          <Link to="/authentication"><button>Login</button></Link>
+          <Link to="/cart"><img  src={cart} alt="cart" /></Link>
           <div className='nav-cart-count'>0</div>
         </div>
     </div>
