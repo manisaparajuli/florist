b30b763007f2bc12ee7683de2b1c20d8e5c7f98a
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-17 14:38:08 +1000

    media query added to hero page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,29 +3,38 @@ import './Navbar.css';
 import logo from '../Assests/bliss-logo.png';
 import cart from '../Assests/add-to-cart.png';
 import { Link } from 'react-router-dom';
+import closeIcon from "../Assests/closeIcon.png";
+import menuIcon from "../Assests/menuIcon.png";
+
+
 
 const Navbar = () => {
   const [activeItem, setActiveItem] = useState('Home'); 
+  const [menuOpen, setMenuOpen] = useState(true);
   const handleItemClick = (item) => {
     setActiveItem(item);
   };
+
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
         <img  src={logo} alt="florist logo here" />
         <p>The florist</p>
+        <div className="menu" onClick={() => {setMenuOpen(!menuOpen)}}>
+        {menuOpen? <img src={menuIcon} alt=""/> : <img src={closeIcon} alt=""/>}
+        </div>
       </div>
-      <div className="menu">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-        <ul className="nav-menu">
+      <div className='nav-item'>
+        <ul className={menuOpen? "nav-menu" : ""} >
           <li onClick={() => handleItemClick('home')} className={activeItem === 'home' ? 'active' : ''}><Link to="/home" style={{textDecoration: "none", color: "inherit"}}> Home</Link></li>
           <li onClick={() => handleItemClick('product')} className={activeItem === 'product' ? 'active' : ''}><Link to="/product" style={{textDecoration: "none", color: "inherit"}}>Product</Link></li>
           <li onClick={() => handleItemClick('wedding&event')} className={activeItem === 'wedding&event' ? 'active' : ''}> <Link to="/events" style={{textDecoration: "none", color: "inherit"}}>Wedding & Event</Link></li>
           <li onClick={() => handleItemClick('aboutUs')} className={activeItem === 'aboutUs' ? 'active' : ''}><Link to="/about" style={{textDecoration: "none", color: "inherit"}}>About Us</Link></li>
+          <li className='login-btn' ><Link to="/authentication" style={{textDecoration: "none", color: "inherit"}}>Login</Link></li>
+
         </ul>
+      </div>
         <div className="nav-login-cart">
           <Link to="/authentication"><button>Login</button></Link>
           <Link to="/cart"><img  src={cart} alt="cart" /></Link>
